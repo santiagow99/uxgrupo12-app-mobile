8aafebaca0e1ebd381e875be2187228056c4c26b
santiago <santiagowilchesv@gmail.com>
2024-03-12 21:00:09 -0500

    Feats login screen

---
+++ app/src/main/java/com/sebastian/project/ui/authentication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sebastian.project.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sebastian.project.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
